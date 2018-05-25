cb48310b5061dee9dcb06439e3b24dcb777e52a3
Paul <paul.pix71@gmail.com>
2018-05-25 22:59:38 +0300

    added copy constructor for color

--- include/color.h
+++ include/color.h
@@ -1,3 +1,8 @@
+/*
+ * https://github.com/plastictown/Engine
+ * Copyright (C) 2018  Mikhail Domchenkov, Paul Sachkov
+ */
+
 #ifndef __COLOR_H__
 #define __COLOR_H__
 
@@ -14,17 +19,79 @@ class color {
   public:
     /// defaut ctor
     color<T, N>();
-    color<T, N> (const std::initializer_list<T> & col);
-    void set(const std::initializer_list<T>& col );
-    void setAt(const size_t index, T&value);
-    T& get(const size_t index);
+    color<T, N> (const std::initializer_list<T> & ilist);
+    ///copy ctor
+    color<T, N>(const color<T, N>& rhs);
+    color<T, N> & operator=(const color<T, N>& rhs );
+    bool operator==(const color<T, N>& rhs );
+    void set(const std::initializer_list<T>& ilist );
+    void setAt(const size_t index, T& value);
+    T& getAt(const size_t index);
 
   protected:
     std::array<T, N> colors;
-
-
-  private:
-
 };
+
+
+template<typename T, size_t N>
+color<T, N>::color() {
+    static_assert( N > 1, "too few coordinates! should be 2 or more");
+    static_assert(std::is_arithmetic
+                  <typename std::remove_cv
+                  < typename std::remove_reference
+                  <T>::type>::type>::value,
+                  "values must have an arithmetic type!");
+    colors.fill(0);
+}
+
+template<typename T, size_t N>
+color<T, N>::color(const std::initializer_list<T> &ilist): color<T, N>() {
+    set(ilist);
+}
+
+template <typename T, size_t N>
+color<T, N>::color(const color<T, N> &rhs) {
+    if(this == &rhs)
+        return;
+    std::copy(rhs.colors.cbegin(), rhs.colors.cend(), colors.begin());
+    return *this;
+
+}
+template<typename T, size_t N>
+color<T, N> &color<T, N>::operator=(const color<T, N> &rhs) {
+    if(this != &rhs) {
+        std::copy(rhs.colors.cbegin(), rhs.colors.cend(), colors.begin());
+    }
+    return * this;
+}
+
+template <typename T, size_t N>
+bool color<T, N>::operator==(const color<T, N> &rhs) {
+    return ((this == &rhs) ? true : (colors == rhs.colors));
+}
+
+template <typename T, size_t N>
+void color<T, N> ::set(const std::initializer_list<T>& ilist) {
+    if(ilist.size() != N)
+        throw std::invalid_argument("color::ctor(std::initializer_list) : invalid number of color parameters");
+    size_t iter = 0;
+    for(auto& i : ilist)
+        colors[iter++] = i;
+}
+
+template<typename T, size_t N>
+void color<T, N>::setAt(const size_t index, T &value) {
+    if(index >= N)
+        throw std::out_of_range("color::setAt(): invalid argument");
+    colors[index] = value;
+}
+
+template<typename T, size_t N>
+T &color<T, N>::getAt(const size_t index) {
+    if(index >= N)
+        throw std::out_of_range("color::getAt(): invalid argument");
+    return colors[index];
+}
+
 
 #endif  //__COLOR_H__
--- main.cpp
+++ main.cpp
@@ -8,32 +8,47 @@
 #include <memory>
 #include <utility>
 #include <exception>
+#include <thread>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/freeglut.h>
+
 #include <engine.h>
 #include <SceneLoader.h>
 #include <polygon.h>
 #include <vertex.h>
+#include <color.h>
 
-#include <thread>
 using namespace std;
+
 int main(int argc, char** argv) try {
-    vertex<float, 3> v{1, 2, 3};
+    color<int, 3> c1 {1, 2, 3};
+    color<int, 3> c2 {1, 2, 3};
+    auto a1 = c1.getAt(0);
+    auto a2 = c1.getAt(1);
+    auto a3 = c1.getAt(2);
+    cout << std::boolalpha << (c1 == c2) << endl;
 
-    v.setAt(0, 8);
+    color<float, 2> c3{1.2, 2.3};
+    auto b1 = c3.getAt(0);
+    auto b2 = c3.getAt(1);
 
-    std::cout << v.getAt(4) << endl;;
 
-    Engine e(argc, argv);
+//    vertex<float, 3> v{1, 2, 3};
 
-    std::shared_ptr<Scene> scn = std::make_shared<Scene>();
-    scn->setVisible(true);
-    e.SetScene(std::move(scn));
+//    v.setAt(0, 8);
 
-    Engine::Run();
+//    std::cout << v.getAt(4) << endl;
 
-    cin.get();
+//    Engine e(argc, argv);
+
+//    std::shared_ptr<Scene> scn = std::make_shared<Scene>();
+//    scn->setVisible(true);
+//    e.SetScene(std::move(scn));
+
+//    Engine::Run();
+
+//    cin.get();
     return 0;
 } catch(std::exception& e) {
     std::cerr << __FUNCTION__ << ": " << e.what() << std::endl;
