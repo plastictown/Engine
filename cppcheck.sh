25ccff38b601e3fdd290839ec42deab2877f6d8c
Mikhail Domchenkov <agplastictown@yandex.ru>
2018-05-28 19:31:50 +0300

    new options in cppcheck

--- cppcheck.sh
+++ cppcheck.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cppcheck --enable=all --language=c++ --platform=unix32 --std=c++11 -v -I./include/ .
+cppcheck --enable=all --inconclusive --language=c++ --platform=unix32 --std=c++11 -I/usr/include -I./include/ .
